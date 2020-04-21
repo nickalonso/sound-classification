c8055c5b29cac8a589e26de09b6229d674f53279
Nick Alonso <43552748+nickalonso@users.noreply.github.com>
2020-04-21 07:45:13 -0400

    Update audio_extract.py

--- audio_extract.py
+++ audio_extract.py
@@ -8,12 +8,15 @@ class WavFileHelper():
         wave_file = open(filename, "rb")
         riff = wave_file.read(12)
         fmt = wave_file.read(36)
+        
         # Extract num_channels
         num_channels_string = fmt[10:12]
         num_channels = struct.unpack('<H', num_channels_string)[0]
+        
         # Extract sample_rate
         sample_rate_string = fmt[12:16]
         sample_rate = struct.unpack("<I", sample_rate_string)[0]
+        
         # Extract bit_depth
         bit_depth_string = fmt[22:24]
         bit_depth = struct.unpack("<H", bit_depth_string)[0]
@@ -32,4 +35,4 @@ for index, row in metadata.iterrows():
     audiodata.append(data)
     
 # Convert to Pandas dataframe
-audiodf = pd.DataFrame(audiodata, columns=['num_channels','sample_rate', 'bit_depth'])
+audiodf = pd.DataFrame(audiodata, columns=['num_channels','sample_rate','bit_depth'])
